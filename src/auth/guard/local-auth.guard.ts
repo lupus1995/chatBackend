52561d66c21afebc6649bb14584209c0fd0e9a87
alexandr panfilov <canya,panfilov.95@gmail.com>
2021-03-21 12:03:51 +0300

    добавил ответ в валидации на запрос авторизации

--- src/auth/guard/local-auth.guard.ts
+++ src/auth/guard/local-auth.guard.ts
@@ -1,5 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { validate, validateSync } from 'class-validator';
+import { AuthDto } from '../dto/auth.dto';
 
 @Injectable()
-export class LocalAuthGuard extends AuthGuard('local') {}
+export class LocalAuthGuard extends AuthGuard('local') {
+  handleRequest(err, user, info, context, status) {
+    console.log(err, user, info, context.args[0].body, status);
+    const authDto = new AuthDto();
+    authDto.email = context.args[0].body.email;
+    authDto.password = context.args[0].body.password;
+
+    const validate = validateSync(authDto);
+
+    if (validate.length > 0) {
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.BAD_REQUEST,
+          message: validate[0],
+          error: 'Bad Request',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    if (err || !user) {
+      throw err || new UnauthorizedException();
+    }
+    return user;
+  }
+}
